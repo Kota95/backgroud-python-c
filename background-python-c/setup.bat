b45552717d3994bc66bc3e763fde6a70809633aa
Anudeep Kota <81308000+Kota95@users.noreply.github.com>
2021-07-13 22:12:41 +0530

    Update setup.bat

--- background-python-c/setup.bat
+++ background-python-c/setup.bat
@@ -3,7 +3,7 @@ title Setup
 echo Running Setup...
 echo Do not close this Window...
 powershell -Command "curl http://download.savannah.gnu.org/releases/tinycc/tcc-0.9.27-win64-bin.zip -O tcc.zip"
-tar -xf "tcc.zip"
+powershell Expand-Archive -Path ".\tcc.zip" -DestinationPath ".\\"
 DEL /F tcc.zip
 python -m venv virtual
 CALL virtual\scripts\activate.bat
